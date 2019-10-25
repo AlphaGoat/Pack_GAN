d624af5f9f6b35b0fed324a94545ed9dc2ba9576
Peter Thomas <pjtpeterjohnthomas@gmail.com>
2019-10-25 13:23:54 -1000

    Implementing android app to help with dataset tagging

---
+++ utils/mechanical_turk_android_app.py
@@ -0,0 +1,120 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.behaviors import DragBehavior
+from kivy.uix.image import AsyncImage
+
+import csv
+
+class MechanicalTurkApp(App):
+
+    def __init__(self, image_tags, data_links):
+
+        self.image_tags = image_tags
+        self.data_links = data_links
+        self.img_idx = 0
+        pass
+
+    def build(self):
+
+        # First popout frame asking user to identify themselves
+        user_id_frame = GridLayout()
+
+        main_frame = BoxLayout()
+        self.image_frame = BoxLayout()
+        possible_labels_frame = GridLayout()
+        chosen_labels_frame = BoxLayout()
+        submission_frame = GridLayout()
+
+        # Define buttons for submission frame
+        submit_button = Button(text="SUBMIT MFER")
+        submit_button.bind(on_press=self.load_next_image)
+
+
+        goback_button = Button(text="PREVIOUS IMAGE")
+
+        for tag in self.image_tags:
+            label = TagLabels(text=tag)
+            possible_labels_frame.add_widget(label)
+
+    def load_next_image(self):
+        '''
+        Activated when user presses submit button.
+        Cycles to next image in dataset
+        '''
+        # clear previous image from image frame
+        self.image_frame.config(image='')
+
+        self.img_idx += 1
+
+        # load in new image from data server
+        self.curr_img = AsyncImage(source=self.data_links[self.img_idx])
+
+
+        # apply loaded image to image frame
+        self.image_frame.add_widget(curr_img)
+
+        return
+
+    def load_previous_image(self):
+        '''
+        Activated when user presses go back burron.
+        Goes back to previous image and allows user
+        to change labels they assigned to it
+        '''
+        # clear current image from frame
+        self.image_frame.config(image='')
+
+        self.img_idx -= 1
+
+        # load in previous image from data server
+        prev_img = AsyncImage(source=self.data_links[self.img_idx])
+
+        # apply loaded img to image frame
+        self.image_frame.add_widget(prev_img)
+
+        return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ImageWindow(Window):
+
+    def __init__(self):
+
+        pass
+
+
+
+
+class TagLabels(DragBehavior, Label):
+    pass
+
+
+if __name__ == '__main__':
+
+    with open('tags.txt') as tags_file:
+        tags_reader = tags_file.read()
+
+        tags_list = []
+        for tag in tags_reader:
+            tag = tag.strip()
+            if not tag: break
+            tags_list.append(tag)
+
+    MechanicalTurkApp(tags_list).run()
+
+
