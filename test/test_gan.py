daf67226872fb1eb778e45486c7ca6baefe111af
AlphaGoat <pjtpeterjohnthomas@gmail.com>
2020-01-01 21:27:35 -0500

    Editing training loop, next step is to integrate loss optimization function into loop

--- test/test_gan.py
+++ test/test_gan.py
@@ -13,6 +13,8 @@ import argparse
 from loss.loss import DRAGANLoss
 from models.generator import Generator
 from models.discriminator import Discriminator
+from dataset.real_dataset_generator import DatasetGenerator
+from dataset.noise_generator import NoiseGenerator
 
 def main(flags):
 
@@ -25,39 +27,54 @@ def main(flags):
 
     # Initialize data generators
     # data generators for real imagery:
-
     num_train_images = flags.num_train_images
     num_valid_images = flags.num_valid_images
     num_test_images = flags.num_test_images
 
-    train_batch_size = flags.train_batch_size
-    valid_batch_size = flags.valid_batch_size
-    test_batch_size = flags.test_batch_size
-
     train_real_data_generator = DatasetGenerator(train_tfrecord_name,
                                                  num_train_images,
-                                                 batch_size=train_batch_size,
+                                                 batch_size=flags.train_batch_size,
                                                  label_smoothing=flags.label_smoothing,
                                                  num_threads=1,
                                                  buffer=30,
                                                  return_filename=False)
+
     train_iterator = train_real_data_generator.get_iterator()
 
     valid_real_data_generator = DatasetGenerator(valid_tfrecord_name,
                                                  num_valid_images,
-                                                 batch_size=valid_batch_size,
+                                                 batch_size=flags.valid_batch_size,
                                                  num_threads=1,
                                                  buffer=30,
                                                  return_filename=False)
+
     valid_iterator = valid_real_data_generator.get_iterator()
 
     test_real_data_generator = DatasetGenerator(test_tfrecord_name,
                                                  num_test_images,
-                                                 batch_size=test_batch_size,
+                                                 batch_size=flags.test_batch_size,
                                                  num_threads=1,
                                                  buffer=30,
                                                  return_filename=False)
+
     test_iterator = test_real_data_generator.get_iterator()
+
+    # grab the resolutions of the images we would like to generate
+    image_width = flags.generated_image_resolution[0]
+    image_height = flags.generated_image_resolution[1]
+    image_channels = flags.generated_image_resolution[2]
+
+    # Initialize noise generator
+    noise_generator = NoiseGenerator(image_height,
+                                     image_width,
+                                     image_channels,
+                                     flags.num_tags,
+                                     flags.latent_space_vector_dim,
+                                     flags.train_batch_size,
+                                     num_threads=1,
+                                     buffer=30)
+
+    noise_iterator = noise_generator.get_iterator()
 
     # Initialize Discriminator and Generator models
 
@@ -65,10 +82,6 @@ def main(flags):
     # summaries generated for the weights of the respective models
     parameter_summary_update_freq = flags.parameter_summary_update_freq
 
-    # First grab the resolutions of the images we would like to generate
-    image_width = flags.generated_image_resolution[0]
-    image_height = flags.generated_image_resolution[1]
-    image_channels = flags.generated_image_resolution[2]
 
     # If a latent space vector dim for input noise for Generator was provided, use
     # that. Otherwise, just use the image width from the provided image resolution
@@ -81,7 +94,7 @@ def main(flags):
     # fetch the number of tags we will use to label/generate imagery
     num_tags = flags.num_tags
 
-    dragan_generator = Generator(image_width,
+    generator = Generator(image_width,
                                  image_height,
                                  image_channels,
                                  latent_space_vector_dim,
@@ -89,7 +102,7 @@ def main(flags):
                                  variable_summary_update_freq=parameter_summary_update_freq,
                                  )
 
-    dragan_discriminator = Discriminator(image_width,
+    discriminator = Discriminator(image_width,
                                          image_height,
                                          image_channels,
                                          num_tags,
@@ -125,10 +138,38 @@ def main(flags):
 
             for step in range(len(train_real_data_generator)):
 
-                # Retrieve next batch of imagery
+                # Retrieve next batch of real imagery
                 train_batch = train_iterator.get_next()
+                real_images, real_tags = train_batch[0], train_batch[1]
 
-                images, tags = train_batch[0], train_batch[1]
+                # Retrieve seed for Generator
+                latent_space_noise, gen_tags = noise_iterator.get_next()
+
+                # concatenate latent space noise and tag vector to feed into generator
+                gen_input = tf.concat([latent_space_noise, gen_tags], axis=1)
+
+                # Now generate fake images
+                gen_images = generator(latent_space_noise, step=step)
+
+                # Feed real data through discriminator and retrieve output
+                # forgery scores as well as label confidences
+                y_real, real_tags_confidences = discriminator(real_images, step=step)
+
+                # Do the same with generated images
+                y_gen, gen_tags_confidences = discriminator(gen_images, step=step)
+
+                # Calculate the losses for the generator and the discriminator
+                discriminator_loss, generator_loss = dragan_loss(real_images,
+                                                                 gen_images,
+                                                                 y_real,
+                                                                 y_gen,
+                                                                 real_tags_confidences,
+                                                                 gen_tags_confidences,
+                                                                 real_tags,
+                                                                 gen_tags)
+
+
+
 
 
 
